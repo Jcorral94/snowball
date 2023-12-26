fd8010250461f07175790302d5fe57ec322c7977
Jay <38401811+Jcorral94@users.noreply.github.com>
2023-12-26 11:05:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const CANVAS = document.querySelector('#canvas');
 const CTX = CANVAS.getContext('2d');
-const CANVAS_BACKGROUND = 'black';
+const CANVAS_BACKGROUND_COLOR = 'black';
+const NUMBER_OF_SNOWBALLS = 2500;
+
 let snowballs;
 let snowBallWorld;
 
 init();
-
+window.addEventListener('resize', () => {
+  canvasSize(innerWidth, innerHeight);
+});
 function init() {
   canvasSize(innerWidth, innerHeight);
   updateCanvas();
-  snowballs = Array.from({ length: 10000 }, (_, i) => {
+  snowballs = Array.from({ length: NUMBER_OF_SNOWBALLS }, (_, i) => {
     return new SnowBall(CTX, Math.random() * CANVAS.width, Math.random() * -1000, Math.random() * 3, CANVAS);
   });
   snowBallWorld = new SnowBalls(snowballs);
@@ -22,7 +26,7 @@ function canvasSize(width, height) {
   CANVAS.height = height;
 }
 function updateCanvas() {
-  CTX.fillStyle = CANVAS_BACKGROUND;
+  CTX.fillStyle = CANVAS_BACKGROUND_COLOR;
   CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
 }
 function draw(timestamp) {
